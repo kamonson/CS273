c9cece441867b381e392311275d10f5b0bb93f29
Kyle <kyle.b.amonson@hotmail.com>
2015-01-06 15:11:24 -0800

    Completed PR1_2 & PR1_3
    created the ability to accept cmd line arguments to change the language
    of hello world statement created this as a new function called pr1_2 and
    added javadocs notation for it,

--- HW1/src/hw1/HW1.java
+++ HW1/src/hw1/HW1.java
@@ -18,16 +18,37 @@ public class HW1 {
         //PR1_1 EX 2.2 creates New ATypeName Object N and output the date retrieved from the ATypeName class
         ATypeName N = new ATypeName();
         System.out.println(N.date);
-        
+
         //PR1_1 EX 2.4 (containing elements of 2.3) Class Data only contains Int I, Float F, Boolen B, create
         //new object d of DataOnly and assign values to i, f and b then print out
         DataOnly d = new DataOnly();
         d.i = 42;
         d.f = 424242424;//hex display
         d.b = true;
-        
+
         System.out.println(d.i);
         System.out.println(d.f);//hex display
         System.out.println(d.b);
-    } 
+        pr1_2(args);//PR 1.2 this calls a function
+    }
+
+    /**
+     * PR 1.3 Allow a user to select language for hello world, choose between
+     * spanish, german, italian
+     */
+    public static void pr1_2(String[] args) {
+
+        cmdLn g = new cmdLn();
+        if (args.length > 0 && args[0].equals("spanish")) {
+            g.greeting = "hola mundo";
+        } else if (args.length > 0 && args[0].equals("german")) {
+            g.greeting = "hallo welt";
+        } else if (args.length > 0 && args[0].equals("italian")) {
+            g.greeting = "Ciao Mondo";
+        }
+        /**
+         * @return the the language chosen for hello world
+         */
+        System.out.println(g.greeting);
+    }
 }
---
+++ HW1/src/hw1/cmdLn.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hw1;
+
+/**
+ *
+ * @author kamonson17
+ */
+//PR_1.2 ex 2.7 class for holding greeting
+public class cmdLn {
+    String greeting = "Hello World!\n";
+}
