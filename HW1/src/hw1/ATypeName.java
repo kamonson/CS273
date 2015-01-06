ea70c0ea50695abb8e9386440c9a1e8b43de5ea2
Zeus <kyle.b.amonson@hotmail.com>
2015-01-06 11:15:22 -0800

    Start of HW1
    HW1 Pr1_1 started, ex 2.2 completed...I think, not completely sure of
    what is was asking for, created a Class of ATypeName, assigned it a
    string variable date containing the date, then created a new object of
    ATypeName N in HW1 class and printed out.  It worked and seemed to full
    fill the requirements

---
+++ HW1/src/hw1/ATypeName.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw1;
+
+/**
+ *
+ * @author kyleb_000
+ */
+//PR1_1 EX 2.2 Creates class ATypeName containing a string variable date containing the date to be called in HW1
+class ATypeName {
+    String date = "Jan 6, 2015\n";    
+} 
---
+++ HW1/src/hw1/HW1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hw1;
+
+/**
+ *
+ * @author kyleb_000
+ */
+public class HW1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //PR1_1 EX 2.2 creates New ATypeName Object N and output the date retrieved from the ATypeName class
+        ATypeName N = new ATypeName();
+        System.out.println(N.date);
+    } 
+}
