0c1c77db32e451c5a34bb73e4e4f1a7fb7c0e885
Zeus <kyle.b.amonson@hotmail.com>
2015-01-19 22:22:15 -0800

    CoinFlipper is flipping
    now I need to make it look good, still need javadocs and deleting old
    info in jcombobox in 4.1

---
+++ SuperBowlCoinFlip/app/src/main/java/t3slasoft/superbowlcoinflip/Flip.java
@@ -0,0 +1,11 @@
+package t3slasoft.superbowlcoinflip;
+
+import java.util.Random;
+
+/**
+ * Created by Kyle on 1/19/2015.
+ */
+public class Flip {
+     Flip(){}
+    public static Integer getFlip(){return (int)(Math.random()*2)+1;}
+}
---
+++ SuperBowlCoinFlip/app/src/main/java/t3slasoft/superbowlcoinflip/MainActivity.java
@@ -0,0 +1,54 @@
+package t3slasoft.superbowlcoinflip;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends ActionBarActivity {
+    Flip coin = new Flip();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v) {
+        if (Flip.getFlip() == 1) {
+            final ImageButton next = (ImageButton) findViewById(R.id.imageButtonCoin);
+            next.setBackgroundResource(R.drawable.heads);
+        } else {
+            final ImageButton next = (ImageButton) findViewById(R.id.imageButtonCoin);
+            next.setBackgroundResource(R.drawable.tails);
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
